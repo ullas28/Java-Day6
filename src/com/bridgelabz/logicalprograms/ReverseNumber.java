2aa301c067edbe26ac026d20015ad1b6788ee971
unknown <you@example.com>
2022-09-23 07:56:25 +0530

    Added ReverseNumber program

---
+++ src/com/bridgelabz/logicalprograms/ReverseNumber.java
@@ -0,0 +1,28 @@
+/*Reverse a number
+In Java, we can reverse a number either by using for loop, while loop, or using recursion.
+The simplest way to reverse a number is by using for loop or while loop. In order to
+reverse a number, we have to follow the following steps:
+a. We need to calculate the remainder of the number using the modulo
+b. After that, we need to multiply the variable reverse by 10 and add the remainder into
+it.
+c. We then divide the number by 10 and repeat steps until the number becomes 0.*/
+
+package com.bridgelabz.logicalprograms;
+
+import java.util.Scanner;
+
+public class ReverseNumber {
+    public static void main(String[] args)
+    {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the number to reverse");
+        int number = s.nextInt(), reverse = 0;
+        while(number != 0)
+        {
+            int remainder = number % 10;
+            reverse = reverse * 10 + remainder;
+            number = number/10;
+        }
+        System.out.println("The reverse of the given number is: " + reverse);
+    }
+}
